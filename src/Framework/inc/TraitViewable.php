ed8b7e80d7edfe025b9ec941a32919286a488a9d
Jakiboy <j.sinnaour.official@gmail.com>
2024-11-05 20:40:57 +0100

    Create TraitViewable.php

---
+++ src/Framework/inc/TraitViewable.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @author     : Jakiboy
+ * @package    : FloatPHP
+ * @subpackage : Helpers Framework Component
+ * @version    : 1.1.0
+ * @copyright  : (c) 2018 - 2024 Jihad Sinnaour <mail@jihadsinnaour.com>
+ * @link       : https://floatphp.com
+ * @license    : MIT
+ *
+ * This file if a part of FloatPHP Framework.
+ */
+
+declare(strict_types=1);
+
+namespace FloatPHP\Helpers\Framework\inc;
+
+use FloatPHP\Helpers\Html\Template;
+
+/**
+ * Define template engine functions.
+ */
+trait TraitViewable
+{
+    /**
+     * Get view environment.
+     * 
+     * @access protected
+     * @inheritdoc
+     */
+    protected function getEnvironment($path, array $options = []) : object
+    {
+        return Template::getEnvironment($path, $options);
+    }
+
+    /**
+     * Extend view callables.
+     * 
+     * @access protected
+     * @inheritdoc
+     */
+    protected function extend(string $name, $callable = null, array $options = []) : object
+    {
+        return Template::extend($name, $callable, $options);
+    }
+}
